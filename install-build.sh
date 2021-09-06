fd44dc9426648b0381f259bcd1cd179982bdca9f
Hoang Anh Nguyen <hoanganhnguyen1994@gmail.com>
2021-09-06 11:22:16 +0700

    create ui version 2

--- install-build.sh
+++ install-build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-cd services/service-management && npm install && npm run build:gateway && npm run build:ingredient && npm run build:recipe
+cd services/service-management && npm install && npm run build:gateway && npm run build:ingredient && npm run build:recipe && cd -
+cd ui/ui-management && npm install && npm run build && cd -
+cd ui/ui-management-v2 && npm install && npm run build && cd -
+docker compose -f docker-compose.build.yaml build
